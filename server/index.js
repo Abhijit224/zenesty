60777b8ad244e41e022505aaa89c6b478dd123a6
Abhijit224 <kalukhe.abhijit@gmail.com>
2022-07-18 08:35:02 -0700

    new node initi

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors=require('cors')
+const bodyParser= require('body-parser')
+const cookieParser=require('cookie-parser')
+const mongoose=require('mongoose')
+const path=require('path')
+const { response } = require('express')
+require('dotenv').config()
+const app=express()
+const connection=process.env.mongodb_url
+
+app.listen(process.env.PORT,(error)=>{
+    if(!error){
+        mongoose.connect(connection,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+     })
+     .then(
+        console.log("Database is connected")
+     )
+    }
+    else{
+        response.json('Database not connected')
+        console.log(error)
+    }
+})
